f7a27e93cad11074f6c09a65e5f2fbedd84e2bca
PenetRaptor <skyjammer159@gmail.com>
2021-09-25 18:12:05 +0700

    Добавил футер и обновил дизайн первго модуля

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,14 +7,16 @@ import { HeaderComponent } from './header/header.component';
 import { Module_1Component } from './module_1/module_1.component';
 import { Module_2Component } from './module_2/module_2.component';
 import { Module_3Component } from './module_3/module_3.component';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
-  declarations: [							
+  declarations: [										
     AppComponent,
       HeaderComponent,
       Module_1Component,
       Module_2Component,
-      Module_3Component
+      Module_3Component,
+      FooterComponent
    ],
   imports: [
     BrowserModule,
